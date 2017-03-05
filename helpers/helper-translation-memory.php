92405a4c57462997e6d0f484400c4aec8dc27668
Yoav Farhi <yoav@farhi.org>
2017-03-05 21:00:51 +0200

    update use of gp-translation-memory class

--- helpers/helper-translation-memory.php
+++ helpers/helper-translation-memory.php
@@ -5,13 +5,11 @@ class Helper_Translation_Memory extends GP_Translation_Helper {
 	public $priority = 1;
 
 	function get_output() {
-		if ( ! class_exists( 'GP_Route_Translation_Memory' ) ) {
+		if ( ! class_exists( 'GP_Translation_Memory' ) ) {
 			return false;
 		}
 
-		$tm_route = new GP_Route_Translation_Memory();
-		$suggestions = $tm_route->suggestions_output_for_original_id( $this->data['original_id'], $this->data['locale_slug'] );
-
+		$suggestions = GP_Translation_Memory::suggestions_output_for_original_id( $this->data['original_id'], $this->data['locale_slug'] );
 		return $suggestions;
 	}
 }
