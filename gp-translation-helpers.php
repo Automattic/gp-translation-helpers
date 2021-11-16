274d1f2db45f3a0280d110d7a88aff018c9c2b0a
Tosin Oguntuyi <tcubed03@gmail.com>
2021-11-16 13:19:06 +0100

    Add a callback that sets comment meta to the comment_post hook

--- gp-translation-helpers.php
+++ gp-translation-helpers.php
@@ -239,3 +239,13 @@ class GP_Translation_Helpers {
 }
 
 add_action( 'gp_init', array( 'GP_Translation_Helpers', 'init' ) );
+add_action( 'comment_post', function( $comment_ID ) {
+	if ( gp_post( 'translation_id' ) ) {
+		$translation_id = sanitize_text_field( gp_post( 'translation_id' ) );
+		add_comment_meta( $comment_ID, 'translation_id', $translation_id );
+	}
+	if ( gp_post( 'comment_locale' ) ) {
+		$comment_locale = sanitize_text_field( gp_post( 'comment_locale' ) );
+		add_comment_meta( $comment_ID, 'locale', $comment_locale  );
+	}
+} );
