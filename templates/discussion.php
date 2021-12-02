2ff52c5d4f554265e3f7058cb6ad25cbf1a9413e
Tosin Oguntuyi <tcubed03@gmail.com>
2021-12-02 16:28:04 +0100

    Extract discussion page to its own template

--- templates/discussion.php
+++ templates/discussion.php
@@ -1,22 +1,3 @@
-<?php
-$breadcrumbs = array(
-	gp_project_links_from_root( $project )
-);
-
-if ( $translation_set ) {
-	gp_title( sprintf( __( 'Discussion &lt; %s &lt; %s &lt; GlotPress', 'glotpress' ), $translation_set->name, $project->name ) );
-	$breadcrumbs[] = $translation_set->name;
-} else {
-	gp_title( sprintf( __( 'Discussion &lt; %s &lt; GlotPress', 'glotpress' ), $project->name ) );
-}
-
-gp_breadcrumb( $breadcrumbs );
-gp_enqueue_scripts( array( 'gp-editor', 'gp-translations-page', 'gp-translation-discussion-js') );
-wp_localize_script( 'gp-translations-page', '$gp_translations_options', array( 'sort' => __( 'Sort', 'glotpress' ), 'filter' => __( 'Filter', 'glotpress' ) ) );
-gp_enqueue_style( 'gp-discussion-css' );
-gp_tmpl_header();
-?>
-
 <h1 class="discussion-heading"><?php echo esc_html( $original->singular ); ?></h1>
 
 <?php if ( $string_translation ) : ?>
@@ -43,34 +24,5 @@ gp_tmpl_header();
 		<?php endif; ?>
 		</h4>
 	<?php endif; ?>
-	
-	<ul class="discussion-list">
-	
-		<?php
-		wp_list_comments( array(
-			'style'       => 'ul',
-			'type'       => 'comment',
-			'callback' => 'gth_discussion_callback',
-			'translation_id' => $translation_id,
-			'original_permalink' => $original_permalink,
-			'locale_slug' => $locale_slug,
-		), $comments );
-		?>
-	</ul><!-- .discussion-list -->
-	<?php
-	comment_form( $args = array(
-		'title_reply'          => __( 'Discuss this string' ),
-		'title_reply_to'       => __( 'Reply to %s' ),
-		'title_reply_before'   => '<h6 id="reply-title" class="discuss-title">',
-		'title_reply_after'    => '</h6>',
-		'id_form'              => 'commentform-' . $post_id,
-		'comment_notes_after'  => implode( "\n",
-			array(
-				'<input type="hidden" name="comment_locale" value="' . esc_attr( $locale_slug ) . '" />',
-				'<input type="hidden" name="translation_id" value="' . esc_attr( $translation_id ) . '" />',
-				'<input type="hidden" name="redirect_to" value="' . esc_url( home_url($_SERVER['REQUEST_URI']) ) . '" />',
-				
-			) ),
-	), $post_id);
-	?>
+	<?php gp_tmpl_load( 'comment-section', get_defined_vars(), dirname( __FILE__ ) ); ?>
 </div>
