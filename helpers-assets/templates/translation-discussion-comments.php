ebb937b79ff28f69a41f417b8020f35892845101
Tosin Oguntuyi <tcubed03@gmail.com>
2021-12-23 14:37:42 +0100

    Allow only logged in users to comment

--- helpers-assets/templates/translation-discussion-comments.php
+++ helpers-assets/templates/translation-discussion-comments.php
@@ -27,18 +27,23 @@
 		?>
 	</ul><!-- .discussion-list -->
 	<?php
-	comment_form( $args = array(
-		'title_reply'          => __( 'Discuss this string' ),
-		'title_reply_to'       => __( 'Reply to %s' ),
-		'title_reply_before'   => '<h6 id="reply-title" class="discuss-title">',
-		'title_reply_after'    => '</h6>',
-		'id_form'              => 'commentform-' . $post_id,
-		'comment_notes_after'  => implode( "\n",
-			array(
-				'<input type="hidden" name="comment_locale" value="' . esc_attr( $locale_slug ) . '" />',
-				'<input type="hidden" name="translation_id" value="' . esc_attr( $translation_id ) . '" />',
-				'<input type="hidden" name="redirect_to" value="' . esc_url( $original_permalink ) . '" />',
-			) ),
-	), $post_id);
+	if ( is_user_logged_in() ) {
+		comment_form( $args = array(
+			'title_reply'          => __( 'Discuss this string' ),
+			'title_reply_to'       => __( 'Reply to %s' ),
+			'title_reply_before'   => '<h6 id="reply-title" class="discuss-title">',
+			'title_reply_after'    => '</h6>',
+			'id_form'              => 'commentform-' . $post_id,
+			'comment_notes_after'  => implode( "\n",
+				array(
+					'<input type="hidden" name="comment_locale" value="' . esc_attr( $locale_slug ) . '" />',
+					'<input type="hidden" name="translation_id" value="' . esc_attr( $translation_id ) . '" />',
+					'<input type="hidden" name="redirect_to" value="' . esc_url( $original_permalink ) . '" />',
+				) ),
+		), $post_id);
+	} else {
+		echo sprintf( __( 'You have to be <a href="%s">logged in</a> to comment.' ), wp_login_url() );
+	}
+	
 	?>
 </div><!-- .discussion-wrapper -->
