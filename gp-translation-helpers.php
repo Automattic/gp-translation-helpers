40ec83010c53a5d389ccea5af734f5af74bdd8c1
Jesús Amieiro <amieiro@gmail.com>
2022-01-20 11:34:55 +0100

    Small bug in the </footer> tab

--- gp-translation-helpers.php
+++ gp-translation-helpers.php
@@ -1,6 +1,32 @@
 <?php
-// Plugin name: GP Translation Helpers
-
+/**
+ * @package gp-translation-helpers
+ */
+/**
+ * Plugin name: GP Translation Helpers
+ * Plugin URI: https://github.com/GlotPress/gp-translation-helpers
+ * Description: GlotPress translation helpers is a GlotPress plugin to
+ * discuss the strings that are being translated in GlotPress.
+ * Version: 0.0.1
+ * Author: the GlotPress team
+ * Author URI: https://glotpress.blog
+ * License: GPLv2 or later
+ * Text Domain: gp-translation-helpers
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 require_once __DIR__ . '/includes/class-gp-route-translation-helpers.php';
 require_once __DIR__ . '/includes/class-gp-translation-helpers.php';
