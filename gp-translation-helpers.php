d380d27fd3c4d9a1e10ff14a256c6daf4c16f3d6
Jesús Amieiro <amieiro@gmail.com>
2022-02-01 19:52:18 +0100

    Small changes in the main file
    Add the required PHP version and tabs in the header

--- gp-translation-helpers.php
+++ gp-translation-helpers.php
@@ -2,16 +2,17 @@
 /**
  * @package gp-translation-helpers
  */
+
 /**
- * Plugin name: GP Translation Helpers
- * Plugin URI: https://github.com/GlotPress/gp-translation-helpers
- * Description: GlotPress translation helpers is a GlotPress plugin to
- * discuss the strings that are being translated in GlotPress.
- * Version: 0.0.1
- * Author: the GlotPress team
- * Author URI: https://glotpress.blog
- * License: GPLv2 or later
- * Text Domain: gp-translation-helpers
+ * Plugin name:     GP Translation Helpers
+ * Plugin URI:      https://github.com/GlotPress/gp-translation-helpers
+ * Description:     GlotPress plugin to discuss the strings that are being translated in GlotPress.
+ * Version:         0.0.2
+ * Requires PHP:    7.2
+ * Author:          the GlotPress team
+ * Author URI:      https://glotpress.blog
+ * License:         GPLv2 or later
+ * Text Domain:     gp-translation-helpers
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -34,14 +35,14 @@ require_once __DIR__ . '/includes/class-gp-translation-helpers.php';
 add_action( 'gp_init', array( 'GP_Translation_Helpers', 'init' ) );
 add_action(
 	'comment_post',
-	function( $comment_ID ) {
+	function( $comment_id ) {
 		if ( gp_post( 'translation_id' ) ) {
 			$translation_id = sanitize_text_field( gp_post( 'translation_id' ) );
-			add_comment_meta( $comment_ID, 'translation_id', $translation_id );
+			add_comment_meta( $comment_id, 'translation_id', $translation_id );
 		}
 		if ( gp_post( 'comment_locale' ) ) {
 			$comment_locale = sanitize_text_field( gp_post( 'comment_locale' ) );
-			add_comment_meta( $comment_ID, 'locale', $comment_locale );
+			add_comment_meta( $comment_id, 'locale', $comment_locale );
 		}
 	}
 );
