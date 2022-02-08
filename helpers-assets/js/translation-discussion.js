6a7f586d0dd26d0c040f5576cdb03f8d38754201
Jesús Amieiro <amieiro@gmail.com>
2022-02-08 16:44:48 +0100

    Improve the JavaScript code to pass the linter

--- helpers-assets/js/translation-discussion.js
+++ helpers-assets/js/translation-discussion.js
@@ -1,62 +1,61 @@
 jQuery( function( $ ) {
-	$('.helper-translation-discussion').on( 'click', '.comments-selector a', function( e ){
+	$( '.helper-translation-discussion' ).on( 'click', '.comments-selector a', function( e ) {
 		e.preventDefault();
-		$('.comments-selector a').removeClass('active-link');
-		$(this).addClass('active-link');
-
-		var $comments = jQuery(e.target).parents('h6').next('.discussion-list');
-		var selector = $(e.target).data('selector');
-		if ( 'all' === selector  ) {
+		$( '.comments-selector a' ).removeClass( 'active-link' );
+		$( this ).addClass( 'active-link' );
+		var $comments = jQuery( e.target ).parents( 'h6' ).next( '.discussion-list' ); // eslint-disable-line vars-on-top
+		var selector = $( e.target ).data( 'selector' ); // eslint-disable-line vars-on-top
+		if ( 'all' === selector ) {
 			$comments.children().show();
 		} else {
 			$comments.children().hide();
 			$comments.children( '.comment-locale-' + selector ).show();
-			$comments.children( '.comment-locale-' + selector ).next('ul').show();
+			$comments.children( '.comment-locale-' + selector ).next( 'ul' ).show();
 		}
 		return false;
 	} );
-	$('.helper-translation-discussion').on( 'submit', '.comment-form', function( e ){
-		e.preventDefault();
-		e.stopImmediatePropagation();
+	$( '.helper-translation-discussion' ).on( 'submit', '.comment-form', function( e ) {
 		var $commentform = $( e.target );
 		var formdata = {
-			content: $commentform.find('textarea[name=comment]').val(),
-			parent: $commentform.find('input[name=comment_parent]').val(),
-			post: $commentform.attr('id').split( '-' )[ 1 ],
+			content: $commentform.find( 'textarea[name=comment]' ).val(),
+			parent: $commentform.find( 'input[name=comment_parent]' ).val(),
+			post: $commentform.attr( 'id' ).split( '-' )[ 1 ],
 			meta: {
-				translation_id : $commentform.find('input[name=translation_id]').val(),
-				locale         : $commentform.find('input[name=comment_locale]').val(),
-				comment_topic  : $commentform.find('select[name=comment_topic]').val(),
-			}
-		}
+				translation_id: $commentform.find( 'input[name=translation_id]' ).val(),
+				locale: $commentform.find( 'input[name=comment_locale]' ).val(),
+				comment_topic: $commentform.find( 'select[name=comment_topic]' ).val(),
+			},
+		};
+		e.preventDefault();
+		e.stopImmediatePropagation();
 
 		if ( formdata.meta.locale ) {
 			/**
 			 * Set the locale to an empty string if option selected has value 'typo' or 'context'
-			 * to force comment to be posted to the English discussion page 
+			 * to force comment to be posted to the English discussion page
 			 */
 			if ( formdata.meta.comment_topic === 'typo' || formdata.meta.comment_topic === 'context' ) {
 				formdata.meta.locale = '';
 			}
 		}
-		
+
 		$.ajax( {
-			url: wpApiSettings.root + 'wp/v2/comments',
+			url: wpApiSettings.root + 'wp/v2/comments', // eslint-disable-line no-undef
 			method: 'POST',
-			beforeSend: function ( xhr ) {
-				xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+			beforeSend: function( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce ); // eslint-disable-line no-undef
 			},
-			data: formdata
-		}).done( function( response ){
+			data: formdata,
+		} ).done( function( response ) {
 			if ( 'undefined' !== typeof ( response.data ) ) {
 				// There's probably a better way, but response.data is only set for errors.
 				// TODO: error handling.
 			} else {
-				$commentform.find('textarea[name=comment]').val('');
-				$gp.translation_helpers.fetch( 'discussion' );
+				$commentform.find( 'textarea[name=comment]' ).val( '' );
+				$gp.translation_helpers.fetch( 'discussion' ); // eslint-disable-line no-undef
 			}
 		} );
 
 		return false;
-	});
-});
+	} );
+} );
--- js/translation-helpers.js
+++ js/translation-helpers.js
@@ -1,68 +1,67 @@
-$gp.translation_helpers = (
+$gp.translation_helpers = ( // eslint-disable-line no-undef
 	function( $ ) {
 		return {
-			init: function( table, fetch_now ) {
-				$gp.translation_helpers.table = table;
-				$gp.translation_helpers.install_hooks();
-				if ( fetch_now ) {
-					$gp.translation_helpers.fetch( false, $('.translations') );
+			init: function( table, fetchNow ) {
+				$gp.translation_helpers.table = table; // eslint-disable-line no-undef
+				$gp.translation_helpers.install_hooks(); // eslint-disable-line no-undef
+				if ( fetchNow ) {
+					$gp.translation_helpers.fetch( false, $( '.translations' ) ); // eslint-disable-line no-undef
 				}
 			},
 			install_hooks: function() {
-				$( $gp.translation_helpers.table )
-					.on( 'beforeShow', '.editor', $gp.translation_helpers.hooks.initial_fetch )
-					.on( 'click', '.helpers-tabs li', $gp.translation_helpers.hooks.tab_select )
-					.on( 'click', 'a.comment-reply-link', $gp.translation_helpers.hooks.reply_comment_form );
+				$( $gp.translation_helpers.table ) // eslint-disable-line no-undef
+					.on( 'beforeShow', '.editor', $gp.translation_helpers.hooks.initial_fetch ) // eslint-disable-line no-undef
+					.on( 'click', '.helpers-tabs li', $gp.translation_helpers.hooks.tab_select ) // eslint-disable-line no-undef
+					.on( 'click', 'a.comment-reply-link', $gp.translation_helpers.hooks.reply_comment_form ); // eslint-disable-line no-undef
 			},
-			initial_fetch : function( $element ) {
-				var $helpers = $element.find('.translation-helpers');
+			initial_fetch: function( $element ) {
+				var $helpers = $element.find( '.translation-helpers' );
 
-				if ( $helpers.hasClass('loaded') || $helpers.hasClass('loading') ) {
+				if ( $helpers.hasClass( 'loaded' ) || $helpers.hasClass( 'loading' ) ) {
 					return;
 				}
 
-				$gp.translation_helpers.fetch( false, $element);
+				$gp.translation_helpers.fetch( false, $element ); // eslint-disable-line no-undef
 			},
-			fetch : function( which, $element ) {
+			fetch: function( which, $element ) {
 				var $helpers;
 				if ( $element ) {
-					$helpers = $element.find('.translation-helpers');
+					$helpers = $element.find( '.translation-helpers' );
 				} else {
-					$helpers = $( '.editor:visible, .translations' ).find('.translation-helpers').first();
+					$helpers = $( '.editor:visible, .translations' ).find( '.translation-helpers' ).first();
 				}
 
-				var originalId  = $helpers.parent().attr('row');
-				var replytocom = $helpers.parent().attr('replytocom');
-				var requestUrl = $gp_translation_helpers_settings.th_url + originalId + '?nohc';
+				var originalId = $helpers.parent().attr( 'row' ); // eslint-disable-line vars-on-top
+				var replytocom = $helpers.parent().attr( 'replytocom' ); // eslint-disable-line vars-on-top
+				var requestUrl = $gp_translation_helpers_settings.th_url + originalId + '?nohc'; // eslint-disable-line
 
 				if ( which ) {
 					requestUrl = requestUrl + '&helper=' + which;
 				}
 				requestUrl = requestUrl + '&replytocom=' + replytocom;
 
-				$helpers.addClass('loading');
+				$helpers.addClass( 'loading' );
 
 				$.getJSON(
 					requestUrl,
-					function( data ){
-						$helpers.addClass('loaded').removeClass('loading');
-						$.each( data, function( id, result ){
-							jQuery('.helpers-tabs li[data-tab="' + id +'"]').find('.count').text( '(' + result.count + ')' );
-							$( '#'  + id ).find('.loading').remove();
-							$( '#'  + id ).find('.async-content').html( result.content );
+					function( data ) {
+						$helpers.addClass( 'loaded' ).removeClass( 'loading' );
+						$.each( data, function( id, result ) {
+							jQuery( '.helpers-tabs li[data-tab="' + id + '"]' ).find( '.count' ).text( '(' + result.count + ')' );
+							$( '#' + id ).find( '.loading' ).remove();
+							$( '#' + id ).find( '.async-content' ).html( result.content );
 						} );
-
 					}
 				);
 			},
 			tab_select: function( $tab ) {
-				var tab_id = $tab.attr('data-tab');
+				var tabId = $tab.attr( 'data-tab' );
 
-				$tab.siblings().removeClass( 'current');
-				$tab.parents('.translation-helpers ').find('.helper').removeClass('current');
+				$tab.siblings().removeClass( 'current' );
+				$tab.parents( '.translation-helpers ' ).find( '.helper' ).removeClass( 'current' );
 
-				$tab.addClass('current');
-				$("#"+tab_id).addClass('current');
+				$tab.addClass( 'current' );
+				$( '#' + tabId ).addClass( 'current' );
 			},
 			reply_comment_form: function( $comment ) {
 				var commentId = $comment.attr( 'data-commentid' );
@@ -75,28 +74,28 @@ $gp.translation_helpers = (
 			},
 			hooks: {
 				initial_fetch: function() {
-					$gp.translation_helpers.initial_fetch( $( this ) );
+					$gp.translation_helpers.initial_fetch( $( this ) ); // eslint-disable-line no-undef
 					return false;
 				},
 				tab_select: function() {
-					$gp.translation_helpers.tab_select( $( this ) );
+					$gp.translation_helpers.tab_select( $( this ) ); // eslint-disable-line no-undef
 					return false;
 				},
-				reply_comment_form: function ( event ) {
+				reply_comment_form: function( event ) {
 					event.preventDefault();
-					$gp.translation_helpers.reply_comment_form( $( this ) );
+					$gp.translation_helpers.reply_comment_form( $( this ) ); // eslint-disable-line no-undef
 					return false;
 				},
-	}
-		}
+			},
+		};
 	}( jQuery )
 );
 
 jQuery( function( $ ) {
-	$gp.translation_helpers.init( $( '.translations' ), true );
-	if ( typeof window.newShowFunctionAttached === 'undefined' ) {
-		window.newShowFunctionAttached = true;
-		var _oldShow = $.fn.show;
+	$gp.translation_helpers.init( $( '.translations' ), true ); // eslint-disable-line no-undef
+	if ( typeof window.newShowFunctionAttached === 'undefined' ) { // eslint-disable-line
+		window.newShowFunctionAttached = true; // eslint-disable-line
+		var _oldShow = $.fn.show; // eslint-disable-line vars-on-top
 		$.fn.show = function( speed, oldCallback ) {
 			return $( this ).each( function() {
 				var obj = $( this ),
@@ -109,6 +108,6 @@ jQuery( function( $ ) {
 				obj.trigger( 'beforeShow' );
 				_oldShow.apply( obj, [ speed, newCallback ] );
 			} );
-		}
+		};
 	}
 } );
